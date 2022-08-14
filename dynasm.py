8904378ce49465f35298908b13e736ad335622e0
intbyte-100 <5656ltl@gmail.com>
2022-08-14 17:54:19 +0300

    add recursive dynasm file preprocessing

--- dynasm.py
+++ dynasm.py
@@ -11,24 +11,58 @@ global dynasm
 global arch
 
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def compile_minilua():
-    print("[Task: minilua compilation]: started")
+    print(Colors.OKGREEN + "[Task: minilua compilation]: started" + Colors.ENDC)
     process = subprocess.Popen(["gcc", "-o", "minilua", "-lm", "minilua.c"], cwd=miniluaPath)
     process.wait()
     if process.returncode != 0:
-        print("[Task: minilua compilation]: failed")
+        print(Colors.FAIL + "[Error: minilua compilation]: failed")
         exit(1)
-    print("[Task: minilua compilation]: finished")
+    print(Colors.OKGREEN + "[Task: minilua compilation]: finished")
 
 
 def minilua_executable():
     if not exists(miniluaPath + "minilua"):
-        print("[Info]: lua interpreter is required")
+        print(Colors.WARNING + "[Info]: lua interpreter is required")
         compile_minilua()
     return miniluaPath + "minilua"
 
 
+def start_preprocessor(file, outfile):
+    if arch is None:
+        print(Colors.FAIL + "[Error: dynasm]: arch is not specified")
+        exit(-1)
+
+    print(Colors.OKGREEN + "[Task: dynasm]: starting preprocessor for", file, Colors.ENDC)
+    process = subprocess.Popen([minilua, dynasm + "preprocessor/dynasm.lua", "-o", outfile, "-D", arch, file])
+    process.wait()
+
+    if process.returncode != 0:
+        print(Colors.FAIL + "[Error: dynasm]: preprocessing failed")
+        return
+
+    print(Colors.OKGREEN + "[Task: dynasm]: preprocessing finished. Output file is", outfile)
+
+
+def output_file_name(src_dir, src_file):
+    return str(src_file).replace(".dasm", "")
+
+
 if __name__ == "__main__":
+    if os.name == "nt":
+        os.system("color")
     src = []
     miniluaPath = os.getcwd() + "/minilua/"
     dynasm = os.getcwd() + "/dynasm/"
@@ -42,9 +76,12 @@ if __name__ == "__main__":
     arch = args.arch
 
     if args.dir is not None:
-        src += list(Path(args.dir).rglob("*.[(dasm.c)(dasm.h]"))
+        src += list(Path(args.dir).rglob("*.dasm.[ch]"))
 
     if args.files is not None:
         src += args.files
 
+    minilua = minilua_executable()
 
+    for file in src:
+        start_preprocessor(file, output_file_name(args.dir, file))
